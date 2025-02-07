8e64feb7a1f94acdf2e5d609cc88ddb1bd534615
Felipe Soares <felipesoarwz@gmail.com>
2025-02-07 18:01:13 -0300

    home page: pt. 2 - desktop and mobile design

---
+++ src/components/ColorInScroll.jsx
@@ -0,0 +1,25 @@
+import { useInView } from "react-intersection-observer";
+
+const ColorInScroll = ({ text }) => {
+  // Usa o hook useInView para detectar quando o elemento está visível
+  const { ref, inView } = useInView({
+    threshold: 0.5, // Define que o callback será chamado quando 50% do elemento estiver visível
+    triggerOnce: false, // Permite que o efeito seja ativado/desativado várias vezes
+  });
+
+  return (
+    <p
+      className="inline"
+      ref={ref} // Referência para o elemento que será observado
+      style={{
+        backgroundColor: inView ? "#fff" : "transparent",
+        color: "#3b45f2",
+        transition: "background-color 0.5s ease", // Adiciona uma transição suave
+      }}
+    >
+      {text}
+    </p>
+  );
+};
+
+export default ColorInScroll;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import ColorInScroll from "../components/ColorInScroll";
+
+import cards from "../assets/images/cards.png";
+import graphic from "../assets/images/graphic.png";
 import "../styles/fonts.css";
 
 const Home = () => {
   return (
-    <div className="lg:mx-[3vw] lg:my-[1.5vw]">
+    <div className="mx-6 my-6 lg:mx-[3vw] lg:my-[1.5vw]">
       <Header />
       <Main />
     </div>
@@ -14,31 +18,31 @@ export default Home;
 const Header = () => {
   return (
     <div className="min-h-screen">
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center">
         <h2 className="lufga-bold lg:text-[1.4vw] pointer-events-none">
-          flashy.
+          starky.
         </h2>
-        <div className="flex justify-between items-center lg:gap-[2.5vw]">
-          <button className="cursor-pointer lg:text-[1.1vw]">
-            Entenda como funciona
+        <div className="flex justify-between gap-[1rem] items-center lg:gap-[2.5vw]">
+          <button className="cursor-pointer text-[.8rem] lg:text-[1.1vw]">
+            <a href="#como-funciona">Como funciona</a>
           </button>
-          <button className="btn-header bg-[#FFFFFF] cursor-pointer transition-all duration-[.3s] ease-in-out lg:rounded-[1.2vw] lg:px-[1.2vw] lg:py-[.4vw] hover:bg-[#131986]">
-            <h2 className="text-[#3b45f2] transition-all duration-[.3s] ease-in-out lg:text-[1.1vw]">
+          <button className="btn-header bg-[#FFFFFF] cursor-pointer transition-all duration-[.3s] ease-in-out rounded-[.8rem] px-[.6rem] lg:rounded-[1.2vw] lg:px-[1.2vw] lg:py-[.4vw] hover:bg-[#131986]">
+            <h2 className="text-[#3b45f2] transition-all duration-[.3s] ease-in-out text-[.8rem] lg:text-[1.1vw]">
               Entrar sem e-mail
             </h2>
           </button>
         </div>
       </div>
       <div className="flex items-center justify-center flex-col h-[70vh]">
-        <h2 className="lufga-bold lg:text-[9vw] relative">
-          flashy.
-          <p className="lufga-med absolute bottom-[1.4vw] lg:left-[.5vw] lg:text-[1.3vw]">
+        <h2 className="lufga-bold relative select-none text-[4rem] lg:text-[9vw] ">
+          starky.
+          <p className="lufga-med absolute text-[.9rem] bottom-[.4rem] lg:bottom-[1.4vw]  lg:left-[.5vw] lg:text-[1.3vw]">
             Repetição inteligente,
           </p>
-          <p className="lufga-med absolute bottom-[-.1vw] lg:left-[.5vw] lg:text-[1.3vw]">
+          <p className="lufga-med absolute text-[.9rem] bottom-[-.4rem] lg:bottom-[-.1vw]   lg:left-[.5vw] lg:text-[1.3vw]">
             Cards customizáveis,
           </p>
-          <p className="lufga-med absolute bottom-[-1.6vw] lg:left-[.5vw] lg:text-[1.3vw]">
+          <p className="lufga-med absolute text-[.9rem] bottom-[-1.3rem] lg:bottom-[-1.6vw]   lg:left-[.5vw] lg:text-[1.3vw]">
             Totalmente de graça.
           </p>
         </h2>
@@ -49,35 +53,82 @@ const Header = () => {
 
 const Main = () => {
   return (
-    <div>
-      <div>
-        <h2 className="lufga-bold lg:text-[1.5vw]">Como funciona o Flashy?</h2>
-      </div>
-      <div className="flex items-top justify-between">
-        <div className="flex flex-col lg:gap-[.7vw] lg:leading-[3.8vw] lg:mt-[1vw]">
-          <h2 className="lg:text-[4vw]">
-            Revise constantemente o que você não sabe{" "}
-            <span className="lufga-bold text-[#fff]">ainda!</span>
-          </h2>
-          <span className="bg-[#ffffff73] block h-[.1vw] w-[40vw] lg:mt-[2vw]"></span>
-          <Item
-            number={"1"}
-            desc={"Adicione um card com a informação que deseja memorizar"}
-          />
-          <Item
-            number={"2"}
-            desc={"Tente se lembrar da informação e confira se acertou"}
-          />
-          <Item
-            number={"3"}
-            desc={"Repita para reforçar sua memória até você saber"}
-          />
-        </div>
+    <div className="flex flex-col gap-[2rem] lg:gap-[1vw]">
+      <div className="lg:min-h-screen" id="como-funciona">
         <div>
-          <h2 className="lg:text-[4vw]">
-            Revise constantemente o que você não sabe{" "}
-            <span className="lufga-bold text-[#fff]">ainda!</span>
+          <h2 className="lufga-bold text-[1.5rem] lg:text-[1.5vw]">
+            Como funciona o Starky?
           </h2>
+        </div>
+        <div className="flex items-top justify-between flex-col gap-[3rem] lg:flex-row">
+          <div className="flex flex-col gap-[1rem] mt-2 lg:gap-[1.3vw] lg:mt-[1vw] ">
+            <h2 className="text-[1.2rem] mb-2 leading-6 lg:text-[4vw] lg:w-[46vw] lg:mb-[2vw] lg:leading-[3.2vw] select-none">
+              Revise constantemente o que você não sabe{" "}
+              <span className="lufga-bold text-[#fff]">ainda!</span>
+            </h2>
+            <Item
+              number={"1"}
+              desc={"Adicione um card com a informação que deseja memorizar"}
+            />
+            <Item
+              number={"2"}
+              desc={"Tente se lembrar da informação e confira se acertou"}
+            />
+            <Item
+              number={"3"}
+              desc={
+                "Avalie a dificuldade do card para ajustar quando você verá novamente"
+              }
+            />
+            <Item
+              number={"4"}
+              desc={"Repita para reforçar sua memória até você saber"}
+            />
+          </div>
+          <div>
+            <img
+              src={cards}
+              alt="cards"
+              className="pointer-events-none lg:w-[38vw]"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="lg:min-h-screen">
+        <h2 className="lufga-bold text-[1.5rem] mt-8 lg:text-[1.5vw]">
+          Por que funciona?
+        </h2>
+        <div className="flex items-top flex-col justify-between gap-[3rem] lg:flex-row lg:gap-[4.5vw]">
+          <div className="flex flex-col gap-[1rem] mt-4 lg:gap-[2vw] lg:mt-[2vw] lg:w-[40vw]">
+            <p className="lufga-reg lg:text-[1.4vw]">
+              Na Starky, utilizamos o sistema de repetição espaçada{" "}
+              <strong className="lufga-bold">
+                <ColorInScroll text={"(SRS - Spaced Repetition System)"} />
+              </strong>
+              , que otimiza a memorização e retenção de informações a longo
+              prazo.
+            </p>
+            <p className="lufga-reg lg:text-[1.4vw]">
+              Ele funciona através de cartões de estudo{" "}
+              <strong className="lufga-bold">
+                <ColorInScroll text={"(cards)"} />
+              </strong>
+              , que são revisados de acordo com a dificuldade que o usuário
+              atribui a cada um.
+            </p>
+            <p className="lufga-reg lg:text-[1.4vw]">
+              Quando você aprende algo pela primeira vez, sua memória desaparece
+              rapidamente, mas se você revisar, isso reforçará sua memória
+              novamente.
+            </p>
+          </div>
+          <div>
+            <img
+              src={graphic}
+              alt="graphic"
+              className="pointer-events-none lg:w-[45vw] lg:translate-y-[-2.5vw]"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -87,13 +138,15 @@ const Main = () => {
 const Item = ({ number, desc }) => {
   return (
     <>
-      <div className="flex items-center justify-start lg:gap-[1vw]">
-        <div className="flex items-center justify-center bg-[#fff] lg:p-[.5vw] lg:w-[2.3vw] lg:h-[2.3vw]">
-          <span className="lufga-bold text-[#3b45f2]">{number}</span>
+      <div className="flex items-center justify-start gap-[1.2rem] lg:gap-[1vw]">
+        <div className="flex items-center justify-center bg-[#fff] min-w-[2rem] min-h-[2rem] p-[.3rem] lg:p-[.5vw] lg:w-[2.3vw] lg:h-[2.3vw]">
+          <span className="lufga-bold text-[#3b45f2] lg:text-[1.1vw]">
+            {number}
+          </span>
         </div>
-        <p>{desc}</p>
+        <p className="lg:text-[1.2vw] leading-5 lg:leading-[2vw]">{desc}</p>
       </div>
-      <span className="bg-[#ffffff73] block h-[.1vw] w-[40vw]"></span>
+      <span className="bg-[#ffffff73] block h-[.1vw] lg:w-[45vw]"></span>
     </>
   );
 };
